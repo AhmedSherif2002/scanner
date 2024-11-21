c95816fad6eb6ab5befae842fe8f4fb1a8ec21db
Ahmed <ahmedsherif602@gmail.com>
2024-11-21 02:54:38 +0200

    scanner done

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,52 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { scan } from './functions';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [file,setFile] = useState(null);
+  const [output,setOutput] = useState([]);
+
+  const fileSelectHandle = (e)=>{
+    console.log(e.target.files[0])
+    setFile(e.target.files[0]);
+  }
+  const scanHandle = (e)=>{
+    console.log(file)
+    console.log("scanning");
+    const tokens = scan(file.path);
+    setOutput(tokens);
+    console.log(tokens);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-full m-auto py-12 flex flex-col items-center gap-8'>
+        <div className='addFile w-1/3 m-auto flex flex-col'>
+          <label htmlFor="file" className='bg-green-500 text-white text-2xl font-semibold rounded-lg text-center cursor-pointer py-10'>Choose File</label>
+          <input type="file" name='file' id='file' className='hidden' onChange={fileSelectHandle}/>
+          <span className={`text-center ${file === null?"hidden":"visible"}`}>Selected file: <span className='text-green-600'>{file?.name}</span></span>
+        </div>
+        <button className='font-bold bg-gray-200 w-fit text-green-700 hover:bg-gray-100 duration-500 text-xl px-4 py-2 rounded-md' onClick={scanHandle}>Submit</button>
+        <div className='tokens-result w-1/3'>
+          <div className='flex flex-row font-semibold bg-green-700 text-white'>
+            <div className='w-1/2 p-2 '>Value</div>
+            <div className='w-1/2 p-2 '>Type</div>
+          </div>
+          <div className='overflow-auto' style={{maxHeight:"400px"}}>
+            {
+              output.map(output=>(
+                <>
+                  <div className='flex flex-row font-semibold bg-gray-100 border-b-2'>
+                    <div className='w-1/2 p-2 '>{output.value}</div>
+                    <div className='w-1/2 p-2 '>{output.type}</div>
+                  </div>
+                </>
+              ))
+            }
+          </div>
+        </div>
       </div>
-      <h1 className='text-red-400'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/functions.js
@@ -0,0 +1,121 @@
+const fs = require("fs");
+
+let states = ["Initial","Number","String","Operator","AssignOp"];
+
+let preserved = {
+    ";":"SEMICOLON",
+    "if":"IF",
+    "then":"THEN",
+    "end":"END",
+    "read":"READ",
+    "write":"WRITE",
+    "repeat":"REPEAT",
+    "until":"UNTIL",
+    ":=":"ASSIGN",
+    "<":"LESSTHAN",
+    "=":"EQUAL",
+    "+":"PLUS",
+    "-":"MINUS",
+    "*":"MULT",
+    "/":"DIV",
+    "(":"OPENBRACKET",
+    ")":"CLOSEBRACKET",
+}
+
+let code = `
+    read x;
+    if 0<x then 
+        fact:=1;
+        repeat
+            fact:=fact*x;
+            x:=x-1
+        until x=0;
+        write fact 
+    end
+`
+
+// let delimiters = [" ",";","\n"];
+let delimiters = [" ","\n","\r"];
+
+let token = "";
+let output = [];
+let currState = "Initial";
+let prevState = "Initial";
+
+const getType = (token) => {
+    if(isNaN(parseInt(token))) return "IDENTIFIER"
+    else return "NUMBER"
+}
+
+const sanitizeToken = (token, state)=>{
+    if(state === "String"){
+        const type = preserved[token]?preserved[token]:"IDENTIFIER";
+        output.push({
+            value: token,
+            type
+        })
+    }
+    if(state === "Number"){
+        output.push({
+            value: token,
+            type: "NUMBER"
+        })
+    }
+    if(state === "AssignOp"){
+        output.push({
+            value: token,
+            type: preserved[token]
+        })
+    }
+    if(state === "Operator"){
+        output.push({
+            value: token,
+            type: preserved[token]
+        })
+    }
+}
+
+const scan = (filepath)=>{
+    console.log(filepath)
+    let code = fs.readFileSync(filepath.toString(),"utf8");
+    console.log(code)
+    code += "\n";
+    for(let c of code){
+        console.log(c);
+        // console.log(c,/[a-zA-Z]/.test(c))
+        if(delimiters.includes(c)){
+            currState = "Initial"
+        }
+        if(/[a-zA-Z]/.test(c)){
+            currState = "String";
+        }
+        if("=+-*/();<".includes(c)){
+            if(c === "=" && currState === "AssignOp"){
+                currState = "AssignOp"
+            }else 
+                currState = "Operator";
+        }
+        if(/[0-9]/.test(c)){
+            currState = "Number";
+        }
+        if(c === ":"){
+            currState = "AssignOp"
+        }
+        if(prevState !== currState){
+            sanitizeToken(token, prevState);
+            token = c;
+        }else{
+            token += c;
+        }
+        prevState = currState;
+    }
+    return output;
+}
+
+// for(token of output){
+//     console.log(token);
+// }
+
+export {
+    scan
+}
