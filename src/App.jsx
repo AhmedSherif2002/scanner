9a5720e9eb93af1320d0b7b61431cdc0dc677aa4
Ahmed <ahmedsherif602@gmail.com>
2024-12-15 17:49:41 +0200

    Routes were fixed to match with electron build

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import "./App.css";
 import TreePage from "./ParserComponents/Tree";
 import Scanner from "./Scanner";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { HashRouter, BrowserRouter, Route, Router, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { Router } from '../lib/electron-router-dom'
+
 
 function App() {
   return(
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Scanner/>}/>
         <Route path="/parser" element={<TreePage/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
